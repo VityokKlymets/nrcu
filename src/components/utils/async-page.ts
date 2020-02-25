2c4fa53488e36d77cfeaba4e0048bad1e982fcb6
vityokklymets <vityokklymets@gmail.com>
2020-02-25 23:53:06 +0200

    hash routing

--- src/components/utils/async-page.ts
+++ src/components/utils/async-page.ts
@@ -1,19 +1,26 @@
 import $ from "jquery"
 import axios from "axios"
 
-interface ILink {
+interface ILinkItem {
   element: HTMLAnchorElement
   href: string
+}
+interface ILink {
+  [hash: string]: ILinkItem
+}
+interface IParseResult {
+  title: string
+  root: JQuery.Node
 }
 type Events = "pageloaded" | "pageloading"
 
 export default class AsyncPage {
-  private links: ILink[]
+  private links: ILink = {}
   private containerRoot: JQuery<HTMLElement>
   private rootId: string
 
   constructor(rootId: string) {
-    this.links = this.init()
+    this.init()
     this.rootId = rootId
     this.containerRoot = $(`#${rootId}`)
 
@@ -26,82 +33,87 @@ export default class AsyncPage {
     pageloading.initEvent(pageloadingType, true, true)
 
     this.addEventListener("pageloaded", () => {
-      this.links = this.init()
+      this.init()
     })
-    
-    window.addEventListener("popstate",async (event: PopStateEvent) => {
-      const href = window.location.pathname
-      await this.loadPage(href)
+    window.addEventListener("hashchange", () => {
+      const hash = window.location.hash
+      this.loadPage(this.links[hash].href)
     })
   }
 
-  private init(): ILink[] {
-    const result: ILink[] = []
-
+  private init() {
     document.querySelectorAll("a").forEach(element => {
-      const href = $(element).attr("href") || ""
+      const $element = $(element)
+      const href = $element.attr("href") || ""
+      const { origin } = window.location
 
-      element.addEventListener("click", (e: Event) => {
-        e.preventDefault()
-        this.loadPage(href)
-      })
+      if (!href || /^#/.test(href) || (/http|https/.test(href) && !new RegExp(origin).test(href))) {
+        return
+      }
 
-      result.push({
-        element,
-        href
-      })
+      const hash = `#${href}`
+      $element.attr("href", hash)
+
+      this.links[hash] = {
+        href,
+        element
+      }
     })
-
-    return result
   }
 
-  private async loadPage(href: string,resetScroll = true) {
-    this.dispatchEvent('pageloading')
-    
-    const responce = await axios.get(href)
+  private parsePage(data: string): IParseResult {
+    const startIndex = data.indexOf("<body>")
+    const endIndex = data.lastIndexOf("</body>")
+    if (!startIndex || !endIndex) {
+      throw new Error("Invalid html")
+    }
 
-    const contentType: string = responce.headers["content-type"]
+    const title = data.substring(
+      data.indexOf("<title>") + "<title>".length,
+      data.indexOf("</title>")
+    )
 
-    if (/text\/html/.test(contentType)) {
-      const data: string = responce.data
-      const startIndex = data.indexOf("<body>")
-      const endIndex = data.lastIndexOf("</body>")
+    const body = data.substring(startIndex + 6, endIndex)
 
-      if (!startIndex || !endIndex) {
-        throw new Error("Invalid html")
-      }
+    const html = $.parseHTML(body, document, true)
 
-      const title = data.substring(
-        data.indexOf("<title>") + "<title>".length,
-        data.indexOf("</title>")
-      )
+    const root = html.find((value: HTMLElement) => {
+      return value.id == this.rootId
+    })
 
-      const body = data.substring(startIndex + 6, endIndex)
-
-      const html = $.parseHTML(body,document,true)
-
-      const root = html.find((value: HTMLElement) => {
-        return value.id == this.rootId
-      })
-      if (!root) throw new Error("root container not found")
-
-      this.containerRoot.html($(root).html())
-      
-      window.history.pushState(null, title, href)
-      
-      if(resetScroll){
-        window.scrollTo(0,0)
-      }
-
-      this.dispatchEvent("pageloaded")
-      
-    } else {
-      throw new Error("Invalid responce")
+    return {
+      root,
+      title
     }
   }
+
+  private async loadPage(href: string, resetScroll = true) {
+    this.dispatchEvent("pageloading")
+    const responce = await axios.get(href)
+    const contentType: string = responce.headers["content-type"]
+
+    if (!new RegExp(/text\/html/).test(contentType)) {
+      throw new Error("Invalid responce")
+    }
+
+    const data: string = responce.data
+    const { root, title } = this.parsePage(data)
+    if (!root) {
+      throw new Error("root container not found")
+    }
+
+    this.containerRoot.html($(root).html())
+
+    if (resetScroll) {
+      window.scrollTo(0, 0)
+    }
+    this.dispatchEvent("pageloaded")
+  }
+
   public addEventListener(type: Events, callback) {
     document.addEventListener(type, callback)
   }
+
   private dispatchEvent(type: Events) {
     document.dispatchEvent(new Event(type))
   }
